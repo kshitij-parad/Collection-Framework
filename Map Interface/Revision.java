7ace8c8fcd0e52e0cc02f1f1affb48e9747299af
kshitij-parad <kshitijparad964@gmail.com>
2025-06-16 22:27:20 +0530

    revision : Hashmap

--- Map Interface/Revision.java
+++ Map Interface/Revision.java
@@ -1,4 +1,5 @@
-package map;
+import java.util.HashMap;
+import java.util.Map;
 
 //🧪 Day 3 – Practice Problem
 /* Problem: Student Directory
@@ -15,6 +16,17 @@ package map;
 */
 public class Revision {
     public static void main(String[] args) {
-        
-    }
+        Map<Integer,String> students = new HashMap<>();
+
+        students.put(101,"Kshitij"); 
+        students.put(102,"Priti"); 
+        students.put(103,"Sanket"); 
+
+       System.out.println("All the Entries: "+ students);
+
+       students.remove(102);
+       System.out.println("does id 101 exists?: "+students.containsKey(101));
+
+        System.out.println("updated Entries: "+ students);
+    }   
 }
