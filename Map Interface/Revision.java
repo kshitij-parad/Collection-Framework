4abe264b42f54562683281216f8526e179a352a5
kshitij-parad <kshitijparad964@gmail.com>
2025-06-16 22:18:34 +0530

    folder rename

---
+++ Map Interface/Revision.java
@@ -0,0 +1,20 @@
+package map;
+
+//🧪 Day 3 – Practice Problem
+/* Problem: Student Directory
+    Write a program using HashMap<Integer, String> to:
+    Add 3 students:
+        101 -> "Kshitij"
+        102 -> "Priti"
+        103 -> "Sanket"
+
+    Print all entries
+    Remove student with ID 102
+    Check if ID 101 exists
+    Print updated directory
+*/
+public class Revision {
+    public static void main(String[] args) {
+        
+    }
+}
